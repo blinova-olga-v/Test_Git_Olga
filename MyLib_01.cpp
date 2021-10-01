8b8498088566a281efb80043f0c6738264ec06fd
Sergey <znseday@yandex.ru>
2021-10-01 14:41:49 +0300

    Comments added

--- MyLib_01.cpp
+++ MyLib_01.cpp
@@ -3,11 +3,17 @@
 
 using namespace std;
 
+///
+/// \brief Print_Sergey_01
+///
 void Print_Sergey_01()
 {
     cout << "Sergey_01" << endl;
 }
 
+///
+/// \brief Print_Sergey_02
+///
 void Print_Sergey_02()
 {
     cout << "Sergey_02" << endl;
